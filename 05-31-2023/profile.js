cc75defbd9bb8f28119664a0a21d1f186d4794be
saikirank033 <saikirank033@gmail.com>
2023-06-01 09:41:53 -0500

    test

---
+++ 05-31-2023/profile.js
@@ -0,0 +1,9 @@
+let myProfile= {
+    name: "saikiran",
+    age: 27,
+    university:"Auburn University at montgomery",
+    Title: "Master's student",
+    Loacation: "Texas",
+    Hobbies: ["Playing", "volleyball","watching", "movies"],
+}
+console.log(myProfile.Hobbies[3]);
